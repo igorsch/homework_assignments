d7ae72376f70e6d29f1435860a0785acd634dac2
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-24 19:19:04 -0700

    completed cash_register

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -2,19 +2,26 @@
 
 class CashRegister
 
-  attr_reader :total
+  def total
+    format('%.2f', @total)
+  end
 
   def initialize
-    @total = 0.0
+    @total = 0.00
   end
 
   def purchase(purcase_ammount)
-    @total += purcase_ammount
+    format('%.2f', @total += purcase_ammount)
   end
 
   def pay(pay_ammount)
     @total -= pay_ammount
-    print "Your change is $#{@total}"
+    if @total > 0
+      puts "Your new total is $#{format('%.2f', @total)}"
+    else
+      puts "Your change is $#{format('%.2f', @total.abs)}"
+      @total = 0.0
+  end
   end
 end
 
@@ -22,6 +29,8 @@ register = CashRegister.new
 puts register.total
 puts register.purchase(3.78)
 puts register.purchase(5.22)
-puts register.pay(5.00)
 puts register.total
+register.pay(5.00)
+puts register.total
+register.pay(5.00)
 puts register.total
