81bf49364864cf115cac8d4ac8c44c5bb22b1a30
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-25 12:41:58 -0700

    completed atm.rb

--- objects/atm.rb
+++ objects/atm.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-puts 'Welcome to Only Bank'
+system("clear")
+puts 'Welcome to Only Bank'.center(40, '-')
 count = 0
 
 def terminate
@@ -8,17 +9,18 @@ def terminate
   exit
 end
 
-# loop do
-#   print 'Please enter your pin: '
-#   pin = gets.chomp.to_i
-#   if pin == 1234
-#     break
-#   else
-#     puts'Incorrect pin.'
-#     count += 1
-#     terminate if count >= 3
-#   end
-# end
+loop do
+  print 'Please enter your pin (Hint: 1234): '
+  pin = gets.chomp.to_i
+  if pin == 1234
+    break
+  else
+    system("clear")
+    puts'Incorrect pin.'.center(27, '-')
+    count += 1
+    terminate if count >= 3
+  end
+end
 
 class Atm
   
@@ -28,7 +30,7 @@ class Atm
   end
   
   def check_balance
-    puts "Your total balance is $#{format('%.2f', @total)}"
+    puts "Your accouunt balance is $#{format('%.2f', @total)}"
   end
   
   def deposit(deposit_ammount)
@@ -37,22 +39,53 @@ class Atm
   end
   
   def withdrawal(withdrawal_ammount)
-    @total += withdrawal_ammount
-    puts "Your new balance is: $#{format('%.2f', @total)}"
+    if withdrawal_ammount > @total
+      system("clear")
+      puts 'Non sufficient funds'.center(40, '`')
+      puts "You have $#{format('%.2f', @total)} in your account.".center(40, ' ')
+    else
+      @total -= withdrawal_ammount
+      puts "Your new balance is: $#{format('%.2f', @total)}" 
+    end
   end
 
   def return_card
-    puts 'Thank you for banking with Only Bank'
+    puts ' Thank you for banking with Only Bank '.center(42, '*')
+    puts ' Have a nice day '.center(42, '*')
     exit
   end
 end
 
 transaction = Atm.new
 
-puts 'What do you want to do'
-#command = gets.chomp
-transaction.check_balance
-transaction.deposit(200.25)
-transaction.withdrawal(345.23)
-transaction.return_card
-    
+loop do
+puts ''
+puts 'Select an option:'.center(40, '*')
+puts 'For Account balance press 1'.center(40)
+puts 'To make a deposit   press 2'.center(40)
+puts 'To withdraw cash    press 3'.center(40)
+puts 'To return your card press 4'.center(40)
+puts '*' * 40
+
+  responce = gets.chomp.to_i
+  if ![1, 2, 3, 4].include? responce
+    system("clear")
+    puts 'Enter a valit option.'
+  elsif responce == 1
+    system("clear")
+    transaction.check_balance
+  elsif responce == 2
+    system("clear")
+    print 'Enter your deposit amount: $ '
+    d_amount = gets.chomp.to_f
+    transaction.deposit(d_amount)
+  elsif responce == 3
+    system("clear")
+    print 'How much do you want to withdraw? $'
+    w_amount = gets.chomp.to_f
+    transaction.withdrawal(w_amount)
+  else
+    system("clear")
+    transaction.return_card
+  end
+end
