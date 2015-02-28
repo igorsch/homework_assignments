d2c5706acf59a206ab3800e1e8c6db4f713f9668
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-27 19:51:23 -0700

    added atm.rb

--- objects/atm.rb
+++ objects/atm.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/env ruby
 
-system("clear")
-puts 'Welcome to Only Bank'.center(40, '-')
+
+def formatted_output(line, offset='-')
+  line.center(42, offset)
+end
+
+system('clear')
+puts '-' * 42
+puts formatted_output('Welcome to Only Bank')
+puts '-' * 42
 count = 0
 
 def terminate
-  puts 'See teller to get your card back.'
+  puts ' Your card was confiscated. '.center(42, '*')
   exit
 end
 
@@ -15,77 +22,83 @@ loop do
   if pin == 1234
     break
   else
-    system("clear")
-    puts'Incorrect pin.'.center(27, '-')
+    system('clear')
+    puts formatted_output('Incorrect pin.')
     count += 1
     terminate if count >= 3
   end
 end
 
+system('clear')
+
 class Atm
   
   def initialize
-    @total = 2432.00
+    @total = rand(2000) + 2000
     format('%.2f', @total)
   end
   
   def check_balance
-    puts "Your accouunt balance is $#{format('%.2f', @total)}"
+    puts "Your account balance is $#{format('%.2f', @total)}"
   end
   
-  def deposit(deposit_ammount)
-    @total += deposit_ammount
+  def deposit(deposit_amount)
+    @total += deposit_amount
     puts "Your new balance is: $#{format('%.2f', @total)}"
   end
   
-  def withdrawal(withdrawal_ammount)
-    if withdrawal_ammount > @total
-      system("clear")
-      puts 'Non sufficient funds'.center(40, '`')
-      puts "You have $#{format('%.2f', @total)} in your account.".center(40, ' ')
+  def withdrawal(withdrawal_amount)
+    if withdrawal_amount > @total
+      system('clear')
+      puts 'Non sufficient funds'
+      puts "You have $#{format('%.2f', @total)} in your account."
     else
-      @total -= withdrawal_ammount
+      @total -= withdrawal_amount
       puts "Your new balance is: $#{format('%.2f', @total)}" 
     end
   end
 
   def return_card
-    puts ' Thank you for banking with Only Bank '.center(42, '*')
-    puts ' Have a nice day '.center(42, '*')
+    puts '-' * 42
+    puts ' Thank you for banking with Only Bank '.center(42, '-')
+    puts ' Have a nice day '.center(42, '-')
+    puts '-' * 42
     exit
   end
+  
+  
 end
 
 transaction = Atm.new
 
 loop do
-puts ''
-puts 'Select an option:'.center(40, '*')
-puts 'For Account balance press 1'.center(40)
-puts 'To make a deposit   press 2'.center(40)
-puts 'To withdraw cash    press 3'.center(40)
-puts 'To return your card press 4'.center(40)
-puts '*' * 40
+puts formatted_output('')
+puts formatted_output('Select an option:')
+puts formatted_output('For Account balance press 1')
+puts formatted_output('To make a deposit   press 2')
+puts formatted_output('To withdraw cash    press 3')
+puts formatted_output('To return your card press 4')
+puts formatted_output('-' * 42)
 
   responce = gets.chomp.to_i
   if ![1, 2, 3, 4].include? responce
-    system("clear")
-    puts 'Enter a valit option.'
+    system('clear')
+    puts 'Enter a valid option.'
   elsif responce == 1
-    system("clear")
+    system('clear')
     transaction.check_balance
   elsif responce == 2
-    system("clear")
-    print 'Enter your deposit amount: $ '
+    system('clear')
+    print 'Enter your deposit amount: $'
     d_amount = gets.chomp.to_f
     transaction.deposit(d_amount)
   elsif responce == 3
-    system("clear")
+    system('clear')
     print 'How much do you want to withdraw? $'
     w_amount = gets.chomp.to_f
     transaction.withdrawal(w_amount)
   else
-    system("clear")
+    system('clear')
     transaction.return_card
   end
 end
