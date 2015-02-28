a80ee2fccd318742c4787371d0392ca71b5905f0
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-27 17:50:45 -0700

    final tweaks for fizz_buzz.rb

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
 
 (1..100).each do |number|
-  if number % 3 == 0 && number % 5 == 0
-    puts 'FizzBuzz'
-  elsif number % 3 == 0
-    puts 'Fizz'
+   if number % 3 == 0  && number % 5 == 0
+     puts "FizzBuzz"
+     `say FizzBuzz`
+     sleep(0.1)
+   elsif number % 3 == 0
+     puts 'Fizz'
+     `say Fizz`
+     sleep(0.1)
   elsif number % 5 == 0
-    puts 'Buzz'
+    puts "Buzz"
+     `say Buzz`
+    sleep(0.1)
   else
     puts number
+     `say #{number}`
+    sleep(0.1)
   end
 end
