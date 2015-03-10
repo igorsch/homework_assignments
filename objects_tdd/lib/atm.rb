2713d262ffcdd8b01c0c9f2d3b4a9021ea73782a
Igor Shcherbak <igorski_is@yahoo.com>
2015-03-09 22:51:27 -0600

    Inproved test method

--- objects_tdd/lib/atm.rb
+++ objects_tdd/lib/atm.rb
@@ -1,8 +1,8 @@
 class Atm
   attr_reader :balance
 
-  def initialize
-    @balance = 2000.00
+  def initialize(balance = rand(2000.00) + 2000.00)
+    @balance = balance
     format('%.2f', @balance)
   end
 
@@ -16,8 +16,10 @@ class Atm
   end
 
   def withdrawal(amount)
-    if amount > @balance.to_i
-      'Non sufficient funds'
+    if amount > balance.to_f
+      output = "Non sufficient funds\n"
+      output << "Your balance is $#{format('%.2f', @balance)}"
+      output
     else
       @balance -= amount
       "Your new balance is: $#{format('%.2f', @balance)}"
--- objects_tdd/spec/lib/atm_spec.rb
+++ objects_tdd/spec/lib/atm_spec.rb
@@ -7,46 +7,48 @@ describe Atm do
 
   describe '#balance' do
     it 'should show a balance of 2000.00' do
-      expect(subject.balance).to eq(2000.00)
+      expect(subject.balance).to be_between(2000.00, 4000.00)
     end
   end
 
   context 'initialized atm' do
-    atm = Atm.new
+    subject { Atm.new(2000) }
     describe '#check_balance' do
       it 'should return "Your account balance is $2000.00"' do
-        expect(atm.check_balance).to eq('Your account balance is $2000.00')
+        expect(subject.check_balance).to eq('Your account balance is $2000.00')
       end
     end
 
     describe '#deposit(amount)' do
       it "should add amount to balance.
         And return 'Your new balance is: $2200.00'" do
-        expect(atm.deposit(200)).to eq('Your new balance is: $2200.00')
+        expect(subject.deposit(200)).to eq('Your new balance is: $2200.00')
       end
     end
+  end
 
-    describe '#withdrawal(amount)' do
-      atm2 = Atm.new
-      context 'for withdrawal less than balance should return balance.' do
-        it "should subtract amount from balance.
+  describe '#withdrawal(amount)' do
+    subject { Atm.new(2000) }
+    context 'for withdrawal less than balance should return balance.' do
+      it "should subtract amount from balance.
         And return 'Your new balance is: $1900.00'" do
-          expect(atm2.withdrawal(100)).to eq('Your new balance is: $1900.00')
-        end
-      end
-
-      context 'for withdrawal greater than balance should raise an error.' do
-        it "should subtract amount from balance.
-        And return 'Non sufficient funds'" do
-          expect(atm2.withdrawal(3100)).to eq('Non sufficient funds')
-        end
+        expect(subject.withdrawal(100)).to eq('Your new balance is: $1900.00')
       end
     end
 
-    describe '#return_card' do
-      it 'should return "Thank you for banking with Only Bank"' do
-        expect(atm.return_card).to eq('Thank you for banking with Only Bank')
+    context 'for withdrawal greater than balance should raise an error.' do
+      it "should subtract amount from balance.
+        And return 'Non sufficient funds'
+        Your balance is $2000.00" do
+        expect(subject.withdrawal(3100)).to eq('Non sufficient funds
+Your balance is $2000.00')
       end
+    end
+  end
+
+  describe '#return_card' do
+    it 'should return "Thank you for banking with Only Bank"' do
+      expect(subject.return_card).to eq('Thank you for banking with Only Bank')
     end
   end
 end
